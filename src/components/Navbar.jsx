e7c9dd134c351cb96216963872c80318c098c55e
olha36 <olhakucheruk36@gmail.com>
2025-08-19 15:39:31 +0300

    finish styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,8 +28,9 @@ const Navbar = () => {
               alt="logo"
               className="w-9 h-9 object-contain"
             />
-            <p className="text-white text-[18px] font-bold cursor-pointer">
-              Olha <span className="sm:block hidden">Kucheruk</span>{" "}
+            <p className="text-white text-[18px] font-bold cursor-pointer flex">
+              Olha &nbsp;
+              <span className="sm:block hidden">Kucheruk</span>
             </p>
           </Link>
           <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -54,7 +55,28 @@ const Navbar = () => {
               onClick={() => setToggle(!toggle)}
             />
 
-            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}></div>
+            <div
+              className={`${
+                !toggle ? "hidden" : "flex"
+              } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            >
+              <ul className="list-none flex  justify-end items-start flex-col gap-4">
+                {navLinks.map((link) => (
+                  <li
+                    key={link.id}
+                    className={`${
+                      active === link.title ? "text-white" : "text-secondary"
+                    } font-poppins font-medium cursor-pointer text-[16px]`}
+                    onClick={() => {
+                      setToggle(!toggle);
+                      setActive(link.title);
+                    }}
+                  >
+                    <a href={`#${link.id}`}>{link.title}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
