c7cd5b645b67720968d64c9a8ab028389c4acd5e
olha36 <olhakucheruk36@gmail.com>
2025-09-08 20:22:19 +0300

    use react hook forms

--- src/styles.js
+++ src/styles.js
@@ -12,6 +12,7 @@ const styles = {
     "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText:
     "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+  swalBtn: "text-[#080808] rounded-md px-6 py-2",
 };
 
 export { styles };
