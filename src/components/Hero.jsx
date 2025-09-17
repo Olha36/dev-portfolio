ef0dd3828cdd4e71664e945c1095318f619581e0
olha36 <olhakucheruk36@gmail.com>
2025-09-17 17:24:21 +0300

    change gradient color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#EFBF04]">Olha</span>
+            Hello, my <br /> name's <span className="text-[#EFBF04]">Olha</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          {/* <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop responsive websites
-          </p>
+          </p> */}
         </div>
       </div>
 
